009b50a4856af9725caf042f4c46df4f43ccc5e8
Mohan Ashapu <ashapumohan123@gmail.com>
2025-09-13 19:49:14 +0530

    Update react-router.config.ts

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,6 @@
-import { vercelPreset } from "@react-router/dev/presets";
 import { defineConfig } from "@react-router/dev/config";
 
 export default {
-  presets: [vercelPreset()],
   appDirectory: './src/app',
   ssr: true,
   prerender: ['/*?'],
