e35a286d96a0b27687d58dc86a52f4745a59a0da
Mohan Ashapu <ashapumohan123@gmail.com>
2025-09-13 19:53:18 +0530

    Update react-router.config.ts

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "@react-router/dev/config";
 
-export default {
+export default defineConfig({
   appDirectory: './src/app',
   ssr: true,
   prerender: ['/*?'],
-} satisfies Config;
+});
